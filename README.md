c426dd9313feaa7ea29086710f643ad042bbd6fd
Jacinda Moore <alynna.kasmira@gmail.com>
2015-11-09 15:13:09 -0500

    iam_policy_describe_ec2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Some terraform modules
 
 * `cloudwatch_elb`: Cloudwatch alarms for an ELB
+* `iam_policy_describe_ec2`: An IAM policy allowing a role to describe things in EC2
---
+++ iam_policy_describe_ec2/main.tf
@@ -0,0 +1,28 @@
+# role name
+variable "role" {}
+
+resource "aws_iam_policy" "describe_ec2" {
+  name = "${var.role}-describe-ec2"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "describe_ec2" {
+  name = "${var.role}-describe-ec2"
+  roles = ["${var.role}"]
+  policy_arn = "${aws_iam_policy.describe_ec2}"
+}
