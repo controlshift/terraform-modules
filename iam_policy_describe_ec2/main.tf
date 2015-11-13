eb2d9d5b8f69e53bf29ab6ad65e6c37d0be992c1
Nathan Woodhull <woodhull@gmail.com>
2015-11-13 15:18:57 -0500

    force into array.

--- iam_policy_describe_ec2/main.tf
+++ iam_policy_describe_ec2/main.tf
@@ -25,6 +25,6 @@ POLICY
 
 resource "aws_iam_policy_attachment" "describe_ec2" {
   name = "${var.app}-${var.env}-describe-ec2"
-  roles = "${split(",", var.roles)}"
+  roles = ["${split(",", var.roles)}"]
   policy_arn = "${aws_iam_policy.describe_ec2.arn}"
 }
