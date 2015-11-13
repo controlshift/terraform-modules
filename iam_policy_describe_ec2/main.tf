a92954478a441ccaf35260f97f21dfa35c7140a0
Nathan Woodhull <woodhull@gmail.com>
2015-11-13 15:02:13 -0500

    transition role to roles

--- iam_policy_describe_ec2/main.tf
+++ iam_policy_describe_ec2/main.tf
@@ -1,5 +1,5 @@
 # role name
-variable "role" {}
+variable "roles" {}
 
 resource "aws_iam_policy" "describe_ec2" {
   name = "${var.role}-describe-ec2"
@@ -23,6 +23,6 @@ POLICY
 
 resource "aws_iam_policy_attachment" "describe_ec2" {
   name = "${var.role}-describe-ec2"
-  roles = ["${var.role}"]
+  roles = "${var.roles}"
   policy_arn = "${aws_iam_policy.describe_ec2.arn}"
 }
