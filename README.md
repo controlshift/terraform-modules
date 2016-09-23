707f30d73e0f39f136767e8b3a2c72a8bd84b8d0
Jacinda Moore <alynna.kasmira@gmail.com>
2016-09-23 10:53:55 -0400

    add cloudwatch_alb_target_group

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Some terraform modules
 
 * `cloudwatch_elb`: Cloudwatch alarms for an ELB
+* `cloudwatch_alb_target_group`: Cloudwatch alarms for a target group behind an Application Load Balancer
 * `iam_policy_describe_ec2`: An IAM policy allowing roles to describe things in EC2
 * `iam_policy_cloudwatch`: An IAM policy allowing roles to manage CloudWatch alarms and send data to CloudWatch
---
+++ cloudwatch_alb_target_group/main.tf
@@ -0,0 +1,87 @@
+variable "alb_dimension_id" {}
+variable "target_group_dimension_id" {}
+variable "app_environment" {}
+variable "targets_name" {}
+variable "server_min_instances" {}
+variable "sns_monitoring_topic_arn" {}
+variable "low_priority_sns_monitoring_topic_arn" {}
+
+resource "aws_cloudwatch_metric_alarm" "healthy_hosts_low_too_long" {
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Healthy Hosts: Long Deficiency"
+  alarm_description = "Less than the desired number of healthy ${var.targets_name} hosts behind the ALB for too long"
+  namespace = "AWS/ApplicationELB"
+  dimensions = {
+    "LoadBalancer" = "${var.alb_dimension_id}"
+    "TargetGroup" = "${var.target_group_dimension_id}"
+  }
+  metric_name = "HealthyHostCount"
+  comparison_operator = "LessThanThreshold"
+  threshold = "${var.server_min_instances}"
+  unit = "Count"
+  period = "300"
+  statistic = "Average"
+  evaluation_periods = "5"
+  alarm_actions = ["${var.sns_monitoring_topic_arn}"]
+  ok_actions = ["${var.sns_monitoring_topic_arn}"]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "healthy_hosts_seriously_low" {
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Healthy Hosts: Severe Deficiency"
+  alarm_description = "Significantly less than the desired number of healthy ${var.targets_name} hosts behind the ALB"
+  namespace = "AWS/ApplicationELB"
+  dimensions = {
+    "LoadBalancer" = "${var.alb_dimension_id}"
+    "TargetGroup" = "${var.target_group_dimension_id}"
+  }
+  metric_name = "HealthyHostCount"
+  comparison_operator = "LessThanThreshold"
+  threshold = "${var.server_min_instances - 1}"
+  unit = "Count"
+  period = "60"
+  statistic = "Average"
+  evaluation_periods = "3"
+  alarm_actions = ["${var.sns_monitoring_topic_arn}"]
+  ok_actions = ["${var.sns_monitoring_topic_arn}"]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "target_response_time" {
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Target Response Time (Latency)"
+  alarm_description = "High latency from backend servers"
+  namespace = "AWS/ApplicationELB"
+  dimensions = {
+    "LoadBalancer" = "${var.alb_dimension_id}"
+    "TargetGroup" = "${var.target_group_dimension_id}"
+  }
+  metric_name = "TargetResponseTime"
+  comparison_operator = "GreaterThanThreshold"
+  threshold = "5"
+  unit = "Seconds"
+  period = "300"
+  statistic = "Average"
+  evaluation_periods = "3"
+  alarm_actions = ["${var.low_priority_sns_monitoring_topic_arn}"]
+  ok_actions = ["${var.low_priority_sns_monitoring_topic_arn}"]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "rejected_connection_count" {
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Rejected Connection Count"
+  alarm_description = "Connections rejected for lack of a healthy target"
+  namespace = "AWS/ApplicationELB"
+  dimensions = {
+    "LoadBalancer" = "${var.alb_dimension_id}"
+    "TargetGroup" = "${var.target_group_dimension_id}"
+  }
+  metric_name = "RejectedConnectionCount"
+  comparison_operator = "GreaterThanThreshold"
+  threshold = "0"
+  unit = "Count"
+  period = "60"
+  statistic = "Sum"
+  evaluation_periods = "1"
+  alarm_actions = ["${var.sns_monitoring_topic_arn}"]
+  ok_actions = ["${var.sns_monitoring_topic_arn}"]
+  insufficient_data_actions = []
+}
