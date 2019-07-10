6938731c8468afcfca56250e6eca376d14a8bb13
Jacinda Moore <jacinda@controlshiftlabs.com>
2019-07-10 16:18:24 -0400

    upgrade openvpn to terraform 0.12

--- openvpn/main.tf
+++ openvpn/main.tf
@@ -5,81 +5,104 @@
 # https://raw.githubusercontent.com/hashicorp/best-practices/master/terraform/modules/aws/network/openvpn/openvpn.tf
 #--------------------------------------------------------------
 
-variable "name"               { default = "openvpn" }
-variable "vpc_id"             { }
-variable "vpc_cidr"           { }
-variable "public_subnet_ids"  { }
-variable "ssl_cert"           { }
-variable "ssl_key"            { }
-variable "ssh_username"       { }
-variable "ami"                { default = "ami-5fe36434" }
-variable "instance_type"      { }
-variable "openvpn_user"       { }
-variable "openvpn_admin_user" { }
-variable "openvpn_admin_pw"   { }
-variable "vpn_cidr"           { }
-variable "route_zone_id"      { }
-variable "route_zone_name"    { }
-variable "app_environment"    { }
-variable "ssh_cidr_block"     { }
+variable "name" {
+  default = "openvpn"
+}
+
+variable "vpc_id" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_ids" {}
+
+variable "ssl_cert" {}
+
+variable "ssl_key" {}
+
+variable "ssh_username" {}
+
+variable "ami" {
+  default = "ami-5fe36434"
+}
+
+variable "instance_type" {}
+
+variable "openvpn_user" {}
+
+variable "openvpn_admin_user" {}
+
+variable "openvpn_admin_pw" {}
+
+variable "vpn_cidr" {}
+
+variable "route_zone_id" {}
+
+variable "route_zone_name" {}
+
+variable "app_environment" {}
+
+variable "ssh_cidr_block" {}
+
 variable "public_hosted_zone_id" {}
 
 resource "aws_security_group" "openvpn" {
-  name   = "${var.name}"
-  vpc_id = "${var.vpc_id}"
+  name = var.name
+  vpc_id = var.vpc_id
   description = "OpenVPN security group"
 
-  tags { Name = "${var.name}" }
+  tags = {
+    Name = var.name
+  }
 
   ingress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["${var.vpc_cidr}"]
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = [var.vpc_cidr]
   }
 
   # For OpenVPN Client Web Server & Admin Web UI
   ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
+    protocol = "tcp"
+    from_port = 443
+    to_port = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # For SSH
   ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["${var.ssh_cidr_block}"]
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    cidr_blocks = [var.ssh_cidr_block]
   }
 
   ingress {
-    protocol    = "udp"
-    from_port   = 1194
-    to_port     = 1194
+    protocol = "udp"
+    from_port = 1194
+    to_port = 1194
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
+    protocol = -1
+    from_port = 0
+    to_port = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "openvpn" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id     = "${element(split(",", var.public_subnet_ids), count.index)}"
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = element(split(",", var.public_subnet_ids), count.index)
   associate_public_ip_address = true
-  vpc_security_group_ids = ["${aws_security_group.openvpn.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.vpn.name}"
+  vpc_security_group_ids = [aws_security_group.openvpn.id]
+  iam_instance_profile = aws_iam_instance_profile.vpn.name
 
-  tags {
-    Name = "${var.name}"
-    environment = "${var.app_environment}"
+  tags = {
+    Name = var.name
+    environment = var.app_environment
     kind = "vpn"
     role = "app"
   }
@@ -91,10 +114,12 @@ admin_user=${var.openvpn_admin_user}
 admin_pw=${var.openvpn_admin_pw}
 USERDATA
 
+
   provisioner "remote-exec" {
     connection {
-      user         = "${var.ssh_username}"
-      host         = "${aws_instance.openvpn.public_ip}"
+      type = "ssh"
+      user = var.ssh_username
+      host = aws_instance.openvpn.public_ip
     }
 
     inline = [
@@ -118,7 +143,6 @@ USERDATA
   }
 }
 
-
 resource "aws_iam_role" "vpn" {
   name = "vpn-${var.app_environment}"
   assume_role_policy = <<POLICY
@@ -136,24 +160,25 @@ resource "aws_iam_role" "vpn" {
   ]
 }
 POLICY
+
 }
 
 resource "aws_iam_instance_profile" "vpn" {
   name = "vpn-${var.app_environment}"
-  roles = ["${aws_iam_role.vpn.name}"]
+  roles = [aws_iam_role.vpn.name]
 }
 
 module "iam_describe_ec2" {
   source = "github.com/controlshift/terraform-modules//iam_policy_describe_ec2"
-  roles = "${aws_iam_role.vpn.name}"
-  env = "${var.app_environment}"
+  roles = aws_iam_role.vpn.name
+  env = var.app_environment
   app = "vpn"
 }
 
 module "iam_manage_cloudwatch" {
   source = "github.com/controlshift/terraform-modules//iam_policy_cloudwatch"
-  roles = "${aws_iam_role.vpn.name}"
-  env = "${var.app_environment}"
+  roles = aws_iam_role.vpn.name
+  env = var.app_environment
   app = "vpn"
 }
 
@@ -186,22 +211,31 @@ resource "aws_iam_policy" "vpn_domain" {
   ]
 }
 POLICY
+
 }
 
 resource "aws_iam_policy_attachment" "vpn_domains_for_hostnames" {
   name = "vpn-${var.app_environment}-manage-domains-for-hostnames"
-  roles = ["${aws_iam_role.vpn.name}"]
-  policy_arn = "${aws_iam_policy.vpn_domain.arn}"
+  roles = [aws_iam_role.vpn.name]
+  policy_arn = aws_iam_policy.vpn_domain.arn
 }
 
 resource "aws_route53_record" "openvpn" {
-  zone_id = "${var.route_zone_id}"
-  name    = "app.${var.route_zone_name}"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.openvpn.public_ip}"]
+  zone_id = var.route_zone_id
+  name = "app.${var.route_zone_name}"
+  type = "A"
+  ttl = "300"
+  records = [aws_instance.openvpn.public_ip]
 }
 
-output "private_ip"  { value = "${aws_instance.openvpn.private_ip}" }
-output "public_ip"   { value = "${aws_instance.openvpn.public_ip}" }
-output "public_fqdn" { value = "${aws_route53_record.openvpn.fqdn}" }
+output "private_ip" {
+  value = aws_instance.openvpn.private_ip
+}
+
+output "public_ip" {
+  value = aws_instance.openvpn.public_ip
+}
+
+output "public_fqdn" {
+  value = aws_route53_record.openvpn.fqdn
+}
