35da5646e4d8fbf2768d2bf4d874152a6564c02e
anero <anero79@gmail.com>
2020-02-25 18:08:15 -0300

    Include region on policies and attachments name to ensure uniqueness across regions

--- iam_policy_cloudwatch/main.tf
+++ iam_policy_cloudwatch/main.tf
@@ -1,12 +1,14 @@
 # role names as string separated by ','
 variable "roles" {}
 
+variable "region" {}
+
 variable "env" {}
 
 variable "app" {}
 
 resource "aws_iam_policy" "manage_cloudwatch" {
-  name = "${var.app}-${var.env}-manage-cloudwatch"
+  name = "${var.app}-${var.region}-${var.env}-manage-cloudwatch"
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -31,7 +33,7 @@ POLICY
 }
 
 resource "aws_iam_policy_attachment" "manage_cloudwatch" {
-  name = "${var.app}-${var.env}-manage-cloudwatch"
+  name = "${var.app}-${var.region}-${var.env}-manage-cloudwatch"
   roles = split(",", var.roles)
   policy_arn = aws_iam_policy.manage_cloudwatch.arn
 }
--- iam_policy_describe_ec2/main.tf
+++ iam_policy_describe_ec2/main.tf
@@ -1,12 +1,14 @@
 # role names as string separated by ','
 variable "roles" {}
 
+variable "region" {}
+
 variable "env" {}
 
 variable "app" {}
 
 resource "aws_iam_policy" "describe_ec2" {
-  name = "${var.app}-${var.env}-describe-ec2"
+  name = "${var.app}-${var.region}-${var.env}-describe-ec2"
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -29,7 +31,7 @@ POLICY
 }
 
 resource "aws_iam_policy_attachment" "describe_ec2" {
-  name = "${var.app}-${var.env}-describe-ec2"
+  name = "${var.app}-${var.region}-${var.env}-describe-ec2"
   roles = split(",", var.roles)
   policy_arn = aws_iam_policy.describe_ec2.arn
 }
