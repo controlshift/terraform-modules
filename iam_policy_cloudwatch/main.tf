f56e9ff52ea5c242a9143c6fc0e7d26d1d8cf9e3
anero <anero79@gmail.com>
2020-02-25 18:30:15 -0300

    Make new region variable optional

--- iam_policy_cloudwatch/main.tf
+++ iam_policy_cloudwatch/main.tf
@@ -1,14 +1,18 @@
 # role names as string separated by ','
 variable "roles" {}
 
-variable "region" {}
+variable "region" { default = "" }
 
 variable "env" {}
 
 variable "app" {}
 
+locals {
+  region_prefix = var.region != "" ? "-${var.region}" : ""
+}
+
 resource "aws_iam_policy" "manage_cloudwatch" {
-  name = "${var.app}-${var.region}-${var.env}-manage-cloudwatch"
+  name = "${var.app}${local.region_prefix}-${var.env}-manage-cloudwatch"
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -33,7 +37,7 @@ POLICY
 }
 
 resource "aws_iam_policy_attachment" "manage_cloudwatch" {
-  name = "${var.app}-${var.region}-${var.env}-manage-cloudwatch"
+  name = "${var.app}${local.region_prefix}-${var.env}-manage-cloudwatch"
   roles = split(",", var.roles)
   policy_arn = aws_iam_policy.manage_cloudwatch.arn
 }
--- iam_policy_describe_ec2/main.tf
+++ iam_policy_describe_ec2/main.tf
@@ -1,14 +1,18 @@
 # role names as string separated by ','
 variable "roles" {}
 
-variable "region" {}
+variable "region" { default = "" }
 
 variable "env" {}
 
 variable "app" {}
 
+locals {
+  region_prefix = var.region != "" ? "-${var.region}" : ""
+}
+
 resource "aws_iam_policy" "describe_ec2" {
-  name = "${var.app}-${var.region}-${var.env}-describe-ec2"
+  name = "${var.app}${local.region_prefix}-${var.env}-describe-ec2"
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -31,7 +35,7 @@ POLICY
 }
 
 resource "aws_iam_policy_attachment" "describe_ec2" {
-  name = "${var.app}-${var.region}-${var.env}-describe-ec2"
+  name = "${var.app}${local.region_prefix}-${var.env}-describe-ec2"
   roles = split(",", var.roles)
   policy_arn = aws_iam_policy.describe_ec2.arn
 }
