564dcf6a39a1f54522fd28bb62d94190ade95aaf
warsucks <kathy@controlshiftlabs.com>
2020-05-08 13:45:27 -0400

    add back alarm for min healthy

--- cloudwatch_alb_target_group/main.tf
+++ cloudwatch_alb_target_group/main.tf
@@ -8,6 +8,8 @@ variable "targets_name" {}
 
 variable "server_min_instances" {}
 
+variable "server_min_healthy" {}
+
 variable "sns_monitoring_topic_arn" {}
 
 variable "low_priority_sns_monitoring_topic_arn" {}
@@ -37,13 +39,33 @@ resource "aws_cloudwatch_metric_alarm" "healthy_hosts_low_too_long" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "healthy_hosts_seriously_low" {
-  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Unhealthy Hosts: At least one"
-  alarm_description = "At least one unhealthy host in ${var.targets_name} behind the ALB. This should not be triggered by normal autoscaling and deployment"
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Healthy Hosts: Severe Deficiency"
+  alarm_description = "Significantly less than the desired number of healthy ${var.targets_name} hosts behind the ALB"
+  namespace = "AWS/ApplicationELB"
+  dimensions = {
+    "LoadBalancer" = var.alb_dimension_id
+    "TargetGroup" = var.target_group_dimension_id
+  }
+  metric_name = "HealthyHostCount"
+  comparison_operator = "LessThanThreshold"
+  threshold = var.server_min_healthy
+  unit = "Count"
+  period = "60"
+  statistic = "Average"
+  evaluation_periods = "3"
+  alarm_actions = [var.sns_monitoring_topic_arn]
+  ok_actions = [var.sns_monitoring_topic_arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "unhealthy_hosts_too_many_too_long" {
+  alarm_name = "${var.app_environment}:alb:public:${var.targets_name} Unhealthy Hosts: Too many"
+  alarm_description = "Too many unhealthy hosts in ${var.targets_name} behind the ALB. This should not be triggered by normal autoscaling and deployment"
 
   metric_name = "UnHealthyHostCount"
   statistic = "Average"
   unit = "Count"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
+  comparison_operator = "GreaterThanThreshold"
   threshold = "1"
   period = "60"
   evaluation_periods = "3"
